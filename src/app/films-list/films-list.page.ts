75e9e775b3c139573e90440674313b2a299e4428
Sikara <daniel.girard@sikara.fr>
2023-12-20 11:53:40 +0100

    IONIC - Ajouter du contenu - Diapo 17

--- src/app/films-list/films-list.page.ts
+++ src/app/films-list/films-list.page.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./films-list.page.scss'],
 })
 export class FilmsListPage implements OnInit {
-
+  films = [
+    { 
+      id: 1,
+      title: 'Film 1',
+      img: '../../assets/img/noimg.png',
+      param1: 'testParam1',
+      param2: 'testParam2'
+    }, { 
+      id: 2,
+      title: 'Film 2',
+      img: '../../assets/img/noimg.png',
+      param1: 'testParam1',
+      param2: 'testParam2'
+    }, { 
+      id: 3,
+      title: 'Film 3',
+      img: '../../assets/img/noimg.png',
+      param1: 'testParam1',
+      param2: 'testParam2'
+    }, { 
+      id: 4,
+      title: 'Film 4',
+      img: '../../assets/img/noimg.png',
+      param1: 'testParam1',
+      param2: 'testParam2'
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
