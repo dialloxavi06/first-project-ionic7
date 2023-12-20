556a315a2c80bbc4f204b0f97d7161eccbff2b32
Sikara <daniel.girard@sikara.fr>
2023-12-20 12:02:03 +0100

    IONIC - Ajouter du contenu - Diapo 18

--- src/app/films-list/film/film.page.ts
+++ src/app/films-list/film/film.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-film',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmPage implements OnInit {
   modif: boolean = false;
-  constructor() { }
+  constructor(
+    private alertCtrl : AlertController
+  ) { }
 
   ngOnInit() {
+  }
+
+  async setModif() {
+    if(!this.modif) {
+      const alert = await this.alertCtrl.create({
+        header : 'Etes vous sur de vouloir modifier ?',
+        subHeader: 'Vous rendrez possible la modification',
+        buttons : [
+          {
+            text: 'Annuler',
+            role: 'Cancel'
+          }, {
+            text: 'Configurer',
+            handler: () => {this.modif = !this.modif}
+          }
+        ]
+      });
+      await alert.present();
+    } else {
+      this.modif = !this.modif;
+    }
   }
 
 }
