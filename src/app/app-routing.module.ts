03393174e11b8d7b896aac4a7a06964cff2511d4
Sikara <daniel.girard@sikara.fr>
2023-12-20 11:25:54 +0100

    IONIC - Ajouter du contenu - Diapo 10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,25 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'films-list',
-    loadChildren: () => import('./films-list/films-list.module').then( m => m.FilmsListPageModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
-  },
-  {
-    path: 'tab',
-    loadChildren: () => import('./tab/tab.module').then( m => m.TabPageModule)
+    loadChildren: () => import('./tab/tab.module').then(m => m.TabPageModule)
   }
 ];
 
--- src/app/tab/tab-routing.module.ts
+++ src/app/tab/tab-routing.module.ts
@@ -6,7 +6,23 @@ import { TabPage } from './tab.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabPage
+    component: TabPage,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      }, {
+        path:'home',
+        loadChildren: () => import('../home/home.module').then(m=>m.HomePageModule)
+      }, {
+        path: 'films',
+        loadChildren: () => import('../films-list/films-list.module').then(m=>m.FilmsListPageModule)
+      }, {
+        path: 'about',
+        loadChildren: () => import('../about/about.module').then(m=>m.AboutPageModule)
+      }
+    ]
   }
 ];
 
